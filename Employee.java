fa99acae8921769d58556e98db20cd87f1ed4368
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-20 12:00:57 +0530

    Checking Employee is Present/Absent

--- Employee.java
+++ Employee.java
@@ -1,5 +1,15 @@
+import java.util.Random;
 public class Employee{
    public static void main (String args[]){
       System.out.println("Welcome to Employee Wage Computation....");
+	  
+	  Random rand = new Random();
+	  int randomCheck=rand.nextInt()%2;
+	  
+	  if(randomCheck==1)
+		  System.out.println("Employee is Present");
+	  else
+		  System.out.println("Employee is Absent");
+	
    }
 }
