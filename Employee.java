8d22bec48a637be807f82f5ddcbc8e50ae0c8db0
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-20 12:23:45 +0530

    Daily Wage calculated

--- Employee.java
+++ Employee.java
@@ -2,14 +2,19 @@ import java.util.Random;
 public class Employee{
    public static void main (String args[]){
       System.out.println("Welcome to Employee Wage Computation....");
-	  
+	  int perHr=20;
+	  int totalWage=0;
+	  int fullDay=0;
 	  Random rand = new Random();
 	  int randomCheck=rand.nextInt()%2;
 	  
-	  if(randomCheck==1)
+	    if(randomCheck==1){
 		  System.out.println("Employee is Present");
-	  else
+		  fullDay=8;}
+	    else{
 		  System.out.println("Employee is Absent");
-	
+		  fullDay=0;}
+	totalWage= perHr * fullDay;
+	System.out.println("Toatl Wage is "+totalWage);
    }
 }
