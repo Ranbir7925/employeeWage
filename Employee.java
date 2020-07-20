1cfd32efee26a8085195947166c00a03008b9c47
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-20 12:31:19 +0530

    Part time Employee Added

--- Employee.java
+++ Employee.java
@@ -6,14 +6,17 @@ public class Employee{
 	  int totalWage=0;
 	  int fullDay=0;
 	  Random rand = new Random();
-	  int randomCheck=rand.nextInt()%2;
+	  int randomCheck=rand.nextInt()%3;
 	  
 	    if(randomCheck==1){
-		  System.out.println("Employee is Present");
-		  fullDay=8;}
+		    System.out.println("Employee is Present for Full Time");
+		    fullDay=8;}
+		else if(randomCheck==2){
+			System.out.println("Employee is Present for Part Time");
+			fullDay=4;}
 	    else{
-		  System.out.println("Employee is Absent");
-		  fullDay=0;}
+		    System.out.println("Employee is Absent");
+		    fullDay=0;}
 	totalWage= perHr * fullDay;
 	System.out.println("Toatl Wage is "+totalWage);
    }
