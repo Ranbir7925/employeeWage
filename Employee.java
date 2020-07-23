bca5d92a23baa4760180e2034a882bce26ca9aca
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-23 14:07:09 +0530

    Monthly wage calculated

--- Employee.java
+++ Employee.java
@@ -1,31 +1,57 @@
 import java.util.Random;
 public class Employee{
-   public static void main (String args[]){
-      System.out.println("Welcome to Employee Wage Computation....");
-	  int perHr=20;
-	  int totalWage=0;
-	  int monthlyWage=0;
-	  int fullDay=0;
-	  int workingDays=20;
-	  Random rand = new Random();
-	  int randomCheck=rand.nextInt()%3;
+	int wagePerHr=20;
+	public void checkAttendance(){
+		int isPresent=1;
+		int isAbsent=0;
+		Random random=new Random();
+		int randomNumber=random.nextInt(2);
+		if (randomNumber==isPresent)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+	public int getDailWage(){
+		int fullDayhrs=8;
+		int dailyWage = wagePerHr * fullDayhrs;
+		return dailyWage;
+	}
+	public int getPartTimeWage(){
+		int partTimeHrs=4;
+		int partTimeWage = wagePerHr * partTimeHrs;
+		return partTimeWage;
+	}
+	public int checkEmployee(int random){
+		int isFullTime=1;
+		int isPartTime=2;
+		switch(random){
+			case 1:
+				return getDailWage();
+			case 2:
+				return getPartTimeWage();
+		}
+		return 0;
+	}
 
-	  switch (randomCheck){
-		  case 1:
-			  System.out.println("Employee is Present for Full Time");
-			  fullDay=8;
-			  break;
-		  case 2:
-			  System.out.println("Employee is Present for Part Time");
-			  fullDay=4;
-			  break;
-		  default:
-			  System.out.println("Employee is Absent");
-			  fullDay=0;
+	public void monthlyWage()
+	{
+	  Random rand = new Random();
+	  int count1=0;
+	  int monthlyWage=0;
+	  int wage=0;
+
+	  while(count1 !=20){
+	  	wage=checkEmployee(rand.nextInt(3));
+	  	monthlyWage+=wage;
+	  	count1++;
 	  }
-	totalWage= perHr * fullDay;
-	monthlyWage= totalWage * workingDays;
-	System.out.println("Total Wage is: "+totalWage);
-	System.out.println("Monthly Wage is: "+monthlyWage);
-   }
+		System.out.println("Monthly Wage is: "+monthlyWage);
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Employee Wage Computation");
+		Employee object=new Employee();
+		object.monthlyWage();
+
+	}
 }
