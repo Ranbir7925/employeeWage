b94847f619ce6d7645fb230173e956d1c92e81df
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-26 13:01:17 +0530

    Save Wages for each Company

--- Employee.java
+++ Employee.java
@@ -1,46 +1,81 @@
+import java.util.*;
 public class Employee {
-	final static int FULL_TIME=2;
-	final static int PART_TIME=1;
-	static int dailyWage=0;
-	static int workHours=0;
-	static int totalWorkingDays=1;
-	static int totalWages=0;
-	static int totalWorkingHrs=0;
-	static String company="";
 
-	//Computes Total Wages  of an Employee in month
-	public static int ComputeWage(int wage_per_hour, int max_working_days, int max_working_hours, String company) {
-		Employee.company=company;
-		while(totalWorkingDays<=max_working_days || totalWorkingHrs<=max_working_days) {
-			int attendence = (int)(Math.random()*3);
+    public static final int isPartTime=1;
+    public static final int isFullTime=2;
 
-			switch(attendence) {
-				case FULL_TIME:
-					workHours=8;
-					break;
-				case PART_TIME:
-					workHours=4;
-					break;
-				default:
-					workHours=0;
-
-			}
-			dailyWage=workHours*wage_per_hour;
-			totalWorkingHrs+=workHours;
-//            System.out.println("Daily Wage of an Employee on Day "+totalWorkingDays+" is "+dailyWage);
-			totalWorkingDays++;
-		}
-		return totalWorkingHrs*wage_per_hour;
-	}
-
-	public static void main(String[] args) {
-
-
-		totalWages=Employee.ComputeWage(15, 4, 10, "Apple");
-		System.out.println("Total wages of Employee in "+Employee.company+" Company ="+totalWages);
-
-		totalWages=Employee.ComputeWage(22, 18, 100, "Jio");
-		System.out.println("Total wages of Employee in "+Employee.company+" Company ="+totalWages);
-
-	}
+    static int ratePerHour;
+    static int maxHours;
+    static int days;
+    static int totalWage;
+    static String company;
+    Employee(String company,int ratePerHour,int days,int maxHours)
+    {
+        this.company=company;
+        this.ratePerHour=ratePerHour;
+        this.days=days;
+        this.maxHours=maxHours;
+    }
+    public static int getWorkingHours(int check)
+    {
+        int hours;
+        switch(check)
+        {
+            case 1:
+                hours=4;
+                break;
+            case 2:
+                hours=8;
+                break;
+            default:
+                hours=0;
+                break;
+        }
+        return hours;
+    }
+    public static int attendanceCheck()
+    {
+        Random rand = new Random();
+        int randomCheck=rand.nextInt()%3;;
+        return randomCheck;
+    }
+    public static void print(int totalDays,int workHours,int totalHours,int dailySalary,int totalSalary)
+    {
+        System.out.println(totalDays+"\t"+workHours+"\t\t"+totalHours+"\t\t"+dailySalary+"\t\t"+totalSalary);
+    }
+    public static void employeeWages()
+    {
+        int check=0,workHours=0,dailySalary=0,totalSalary=0,totalHours=0,totalDays=0;
+        System.out.println("Day\tDaily Hours\tTotal Hours\tDaily Wage\tTotal Wage");
+        while(totalHours<maxHours && totalDays<days)
+        {
+            totalDays+=1;
+            check=attendanceCheck();
+            workHours=getWorkingHours(check);
+            totalHours=totalHours+workHours;
+            dailySalary=workHours*ratePerHour;
+            totalSalary=totalHours*ratePerHour;
+            print(totalDays,workHours,totalHours,dailySalary,totalSalary);
+        }
+        totalWage= totalSalary;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number of companies:");
+        int noOfCompanies=sc.nextInt();
+        for(int counter=1;counter<=noOfCompanies;counter++)
+        {
+            System.out.println("Enter name of company:");
+            String company=sc.next();
+            System.out.println("Enter rate per hour:");
+            int ratePerHour=sc.nextInt();
+            System.out.println("Enter number of working days:");
+            int days=sc.nextInt();
+            System.out.println("Enter maximum working hours:");
+            int maxHours=sc.nextInt();
+            Employee companyname=new Employee(company,ratePerHour,days,maxHours);
+            companyname.employeeWages();
+            System.out.println("Employee wage in company "+companyname.company+" is "+companyname.totalWage);
+        }
+    }
 }
