79c668eee217d28810b70b325b6166c21b0690e2
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-07-28 12:48:22 +0530

    Stored Daily Wage along With Total Wage

--- CompanyEmployeeWage.java
+++ CompanyEmployeeWage.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class CompanyEmployeeWage
 {
 	public static final int isPartTime=1;
@@ -8,12 +10,14 @@ public class CompanyEmployeeWage
     public int days;
     public int maxHours;
     public int totalWage;
+    public ArrayList<Integer> dailWage;
     CompanyEmployeeWage(String company,int ratePerHour,int days,int maxHours)
     {
         this.company=company;
         this.ratePerHour=ratePerHour;
         this.days=days;
         this.maxHours=maxHours;
+        dailWage=new ArrayList<Integer>();
     }
     public void setTotalWage(int totalWage)
     {
@@ -23,5 +27,9 @@ public class CompanyEmployeeWage
     public String toString()
     {
     	return "Employee wage in company "+company+" is "+totalWage;
+    }
+    public void addDailyWage(int dailySalary)
+    {
+        this.dailWage.add(dailySalary);
     }
 }
--- Employee.java
+++ Employee.java
@@ -27,7 +27,7 @@ public class Employee implements EmployeeWage {
     }
 
 
-    public static int getWorkingHours(int check) {
+    public int getWorkingHours(int check) {
         int hours;
         switch (check) {
             case 1:
@@ -43,26 +43,28 @@ public class Employee implements EmployeeWage {
         return hours;
     }
 
-    public static int attendanceCheck() {
+    public int attendanceCheck() {
         int result = (int) Math.floor((Math.random() * 10) % 3);
         return result;
     }
 
-    public static void print(int totalDays, int workHours, int totalHours, int dailySalary, int totalSalary) {
-        System.out.println(totalDays + "\t" + workHours + "\t\t" + totalHours + "\t\t" + dailySalary + "\t\t" + totalSalary);
-    }
-
-    public static int employeeWages(CompanyEmployeeWage companyEmployee) {
+    public int employeeWages(CompanyEmployeeWage companyEmployee)
+    {
         int check = 0, workHours = 0, dailySalary = 0, totalSalary = 0, totalHours = 0, totalDays = 0;
         System.out.println("Day\tDaily Hours\tTotal Hours\tDaily Wage\tTotal Wage");
-        while (totalHours < companyEmployee.maxHours && totalDays < companyEmployee.days) {
+        while (totalHours < companyEmployee.maxHours && totalDays < companyEmployee.days)
+        {
             totalDays += 1;
             check = attendanceCheck();
             workHours = getWorkingHours(check);
             totalHours = totalHours + workHours;
             dailySalary = workHours * companyEmployee.ratePerHour;
-            totalSalary = totalHours * companyEmployee.ratePerHour;
-            print(totalDays, workHours, totalHours, dailySalary, totalSalary);
+            companyEmployee.addDailyWage(dailySalary);
+        }
+        totalSalary = totalHours * companyEmployee.ratePerHour;
+        for (int i=0;i<companyEmployee.dailWage.size();i++)
+        {
+            System.out.println("Wage on day "+(i+1)+" is "+companyEmployee.dailWage.get(i));
         }
         return totalSalary;
     }
@@ -71,7 +73,7 @@ public class Employee implements EmployeeWage {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number of companies:");
         int noOfCompanies = sc.nextInt();
-        Employee employeeWageArray = new Employee(noOfCompanies);
+        EmployeeWage  employeeWageList = new EmployeeWage(noOfCompanies);
 
         for (int counter = 1; counter <= noOfCompanies; counter++) {
             System.out.println("Enter name of company:");
@@ -82,9 +84,9 @@ public class Employee implements EmployeeWage {
             int days = sc.nextInt();
             System.out.println("Enter maximum working hours:");
             int maxHours = sc.nextInt();
-            employeeWageArray.addCompany(company, ratePerHour, days, maxHours);
+            employeeWageList.addCompany(company, ratePerHour, days, maxHours);
         }
-        employeeWageArray.computeWage();
+        employeeWageList.computeWage();
     }
 
 }
